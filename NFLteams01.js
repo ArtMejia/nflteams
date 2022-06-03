2721e945f6fc3d11b64b107846b26da98d253476
ArtMejia <arteniomejia.student@careerdevs.com>
2022-06-02 21:19:59 -0400

    first commit

---
+++ NFLteams01.js
@@ -0,0 +1,7 @@
+const nflTeamsAfc = ['Patriots', 'Chiefs', 'Bucs', 'Titans', 'Bills', 'Bengals', 'Raiders', 'Steelers', 'Colts', 'Dolphins', 'Chargers', 'Browns', 'Ravens', 'Broncos', 'Jets', 'Texans', 'Jaguars']
+
+for (let i = 0; i < nflTeamsAfc.length; i++) {
+    if(nflTeamsAfc[i].includes('a') ) {
+        console.log(nflTeamsAfc[i]);
+    }
+}
